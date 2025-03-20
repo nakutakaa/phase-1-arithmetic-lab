f082f57f41f49837376230e16b056279bd286699
Mkalabai John <nakutakaa@gmail.com>
2025-03-20 23:23:25 +0300

    arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 62
+const num2 = 1
+const multiply = num1 * num2
+console.log(multiply);
+const random = Math.floor(Math.random() * 20)+3;
+console.log(random);
+const num3 = 29
+const num4 = 5
+const mod = (num3 % num4);
+console.log(mod);
+const max = Math.max(17, 18, 19, 20);
+console.log(max);
